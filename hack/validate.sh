f73af38cbfb66f307967b2327b1f7db422ee2492
Marcos Soutullo Rodriguez <marcos.soutullo91@gmail.com>
2022-04-15 21:40:18 +0100

    chore: format SH scripts

--- hack/validate.sh
+++ hack/validate.sh
@@ -34,19 +34,17 @@ echo "INFO - Downloading Flux OpenAPI schemas"
 mkdir -p /tmp/flux-crd-schemas/master-standalone-strict
 curl -sL https://github.com/fluxcd/flux2/releases/latest/download/crd-schemas.tar.gz | tar zxf - -C /tmp/flux-crd-schemas/master-standalone-strict
 
-find . -type f -name '*.yaml' -print0 | while IFS= read -r -d $'\0' file;
-  do
-    echo "INFO - Validating $file"
-    yq e 'true' "$file" > /dev/null
+find . -type f -name '*.yaml' -print0 | while IFS= read -r -d $'\0' file; do
+  echo "INFO - Validating $file"
+  yq e 'true' "$file" >/dev/null
 done
 
 echo "INFO - Validating cluster"
-find ./cluster -maxdepth 2 -type f -name '*.yaml' -print0 | while IFS= read -r -d $'\0' file;
-  do
-    kubeval ${file} --strict --ignore-missing-schemas --additional-schema-locations=file:///tmp/flux-crd-schemas
-    if [[ ${PIPESTATUS[0]} != 0 ]]; then
-      exit 1
-    fi
+find ./cluster -maxdepth 2 -type f -name '*.yaml' -print0 | while IFS= read -r -d $'\0' file; do
+  kubeval ${file} --strict --ignore-missing-schemas --additional-schema-locations=file:///tmp/flux-crd-schemas
+  if [[ ${PIPESTATUS[0]} != 0 ]]; then
+    exit 1
+  fi
 done
 
 # mirror kustomize-controller build options
@@ -54,12 +52,11 @@ kustomize_flags="--load-restrictor=LoadRestrictionsNone --reorder=legacy"
 kustomize_config="kustomization.yaml"
 
 echo "INFO - Validating kustomize overlays"
-find . -type f -name $kustomize_config -print0 | while IFS= read -r -d $'\0' file;
-  do
-    echo "INFO - Validating kustomization ${file/%$kustomize_config}"
-    kustomize build "${file/%$kustomize_config}" $kustomize_flags | \
-      kubeval --ignore-missing-schemas --strict --additional-schema-locations=file:///tmp/flux-crd-schemas
-    if [[ ${PIPESTATUS[0]} != 0 ]]; then
-      exit 1
-    fi
+find . -type f -name $kustomize_config -print0 | while IFS= read -r -d $'\0' file; do
+  echo "INFO - Validating kustomization ${file/%$kustomize_config/}"
+  kustomize build "${file/%$kustomize_config/}" $kustomize_flags |
+    kubeval --ignore-missing-schemas --strict --additional-schema-locations=file:///tmp/flux-crd-schemas
+  if [[ ${PIPESTATUS[0]} != 0 ]]; then
+    exit 1
+  fi
 done
